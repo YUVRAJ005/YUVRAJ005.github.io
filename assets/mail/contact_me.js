74a083396d82adb41f967f7849ed51768ea6ca43
Abhishek Iche <58034199+YUVRAJ005@users.noreply.github.com>
2023-06-13 14:55:13 +0530

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -57,7 +57,7 @@ $(function () {
                         $("<strong>").text(
                             "Sorry " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", it seems that mail server is not responding. Please try again later!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
